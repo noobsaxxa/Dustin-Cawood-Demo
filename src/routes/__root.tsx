be0dfb44366c3e232044858baeb0001f57a0afb7
noobsaxxa <munim@iclosed.io>
2026-09-22 02:48:12 +0500

    Add DC favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -84,6 +84,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
+      { rel: "icon", href: "/dc-icon.svg", type: "image/svg+xml" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@500;600;700&family=Manrope:wght@400;500;600&display=swap" },
